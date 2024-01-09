01bb1d3bca7bb76f4cd9b1b6be4297c7b35da5ab
ivansalinas <ivansalinasgarcia@gmail.com>
2024-01-09 13:47:36 +0100

    added gatsby-plugin-manifest

--- calculator-app-main/gatsby-config.js
+++ calculator-app-main/gatsby-config.js
@@ -2,9 +2,23 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Calculator App`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-offline"]
+    siteMetadata: {
+        title: `Calculator App`,
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    plugins: [
+        'gatsby-plugin-postcss',
+        {
+          resolve: `gatsby-plugin-manifest`,
+          options: {
+            name: `GatsbyJS`,
+            short_name: `GatsbyJS`,
+            start_url: `/`,
+            background_color: `#f7f0eb`,
+            theme_color: `#a2466c`,
+            display: `standalone`,
+          },
+        },
+        'gatsby-plugin-offline',
+    ],
 };
