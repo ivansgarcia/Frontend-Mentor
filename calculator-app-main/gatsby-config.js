4f819a498bfe9100cb06c566fef7a4102bc53155
ivansalinas <ivansalinasgarcia@gmail.com>
2024-01-09 14:14:14 +0100

    fix facivon url

--- calculator-app-main/gatsby-config.js
+++ calculator-app-main/gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
             background_color: `#f7f0eb`,
             theme_color: `#a2466c`,
             display: `standalone`,
-            icon: '/favicon.ico',
+            icon: 'static/favicon.ico',
           },
         },
         'gatsby-plugin-offline',
